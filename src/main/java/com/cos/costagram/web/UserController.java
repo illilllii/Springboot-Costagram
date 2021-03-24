7084e71d557be30ee8226f60300713be54749e03
sksdl9749 <sksdl9749@gmail.com>
2021-03-24 09:47:24 +0900

    9.인스타그램 클론코딩 - Comment, Follow 모델 생성완료

--- src/main/java/com/cos/costagram/web/UserController.java
+++ src/main/java/com/cos/costagram/web/UserController.java
@@ -3,7 +3,6 @@ package com.cos.costagram.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
