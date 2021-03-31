a2ba7c9fae253d326679ab09c4cc0e82a13009c3
sksdl9749 <sksdl9749@gmail.com>
2021-03-31 11:35:25 +0900

    14. facebook 로그인 완료

--- src/main/java/com/cos/costagram/config/auth/PrincipalDetails.java
+++ src/main/java/com/cos/costagram/config/auth/PrincipalDetails.java
@@ -2,20 +2,30 @@ package com.cos.costagram.config.auth;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import com.cos.costagram.domain.user.User;
 
 import lombok.Data;
 
 @Data
-public class PrincipalDetails implements UserDetails{
+public class PrincipalDetails implements UserDetails, OAuth2User{
 
 	private User user;
+	private Map<String, Object> attributes; // OAuth 제공자로 부터 받은 회원 정보
+	private boolean isOauth = false;
+	
 	public PrincipalDetails(User user) {
 		this.user = user;
+	}
+	public PrincipalDetails(User user, Map<String, Object> attributes, boolean isOauth) {
+		this.user = user;
+		this.attributes = attributes;
+		this.isOauth = isOauth;
 	}
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -23,7 +33,11 @@ public class PrincipalDetails implements UserDetails{
 		collectors.add(()->"ROLE_"+user.getRole().toString());
 		return collectors;
 	}
-
+	@Override
+	public Map<String, Object> getAttributes() {
+		return attributes;
+	}
+	
 	@Override
 	public String getPassword() {
 		
@@ -54,6 +68,11 @@ public class PrincipalDetails implements UserDetails{
 	@Override
 	public boolean isEnabled() {		
 		return true;
+	}
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
